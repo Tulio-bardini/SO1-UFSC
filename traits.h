ee8340c0c11c177e4d7fd44bde2a37005e4066a6
Enzo <enzobassani09@gmail.com>
2022-09-18 22:13:07 -0300

    Update - working debugs

--- traits.h
+++ traits.h
@@ -10,15 +10,33 @@
 __BEGIN_API
 
 class CPU; //declaração das classes criadas nos trabalhos devem ser colocadas aqui
+class Debug;
+class System;
 
 //declaração da classe Traits
 template<typename T>
 struct Traits {
+    static const bool debugged = false;
 };
 
 template<> struct Traits<CPU>
 {
     static const unsigned int STACK_SIZE = 65536;
+};
+
+template <>
+struct Traits<Debug> : public Traits<void>
+{
+    static const bool error = false;
+    static const bool warning = false;
+    static const bool info = false;
+    static const bool trace = true;
+};
+
+template <>
+struct Traits<System> : public Traits<void>
+{
+    static const bool debugged = false;
 };
 
 __END_API
