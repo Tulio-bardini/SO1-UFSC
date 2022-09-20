cd7fc7febb2ba34794b11b83955d32bf828b86df
Tulio-bardini <tulio.bardini53@gmail.com>
2022-09-19 23:06:31 -0300

    Update debug messages

--- thread.cc
+++ thread.cc
@@ -11,6 +11,8 @@ Thread *Thread::_running = 0;
 
 int Thread::switch_context(Thread * prev, Thread * next)
 {
+    db<Thread>(TRC) << "Thread::switch_context() chamado\n";
+    db<Thread>(INF) << "switch_context: id thread_prev=" + prev->_id + " / id thread_next=" + next->_id "\n";
     CPU::switch_context(prev->_context, next->_context);
     _running = next;
     return 0;
@@ -18,11 +20,13 @@ int Thread::switch_context(Thread * prev, Thread * next)
 
 void Thread::thread_exit (int exit_code)
 {
+    db<Thread>(TRC) << "Thread::thread_exit() chamado\n";
     delete _context;
 }
 
 int Thread::id()
 {
+    db<Thread>(TRC) << "Thread::id() chamado\n";
     return _id;
 }
 
